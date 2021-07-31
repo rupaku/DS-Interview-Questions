1baecabcae0f0137a832a2cce2a495dfccee9603
rupaku <rupakumari201@yahoo.com>
2021-07-31 13:56:28 +0530

    number of islands using BFS and DFS

--- Google/Tree and Graph/Q53NumOfIsland.py
+++ Google/Tree and Graph/Q53NumOfIsland.py
@@ -1,6 +1,7 @@
 '''
 https://leetcode.com/problems/number-of-islands/submissions/
 '''
+#DFS way
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         H=len(grid)
@@ -28,3 +29,35 @@ class Solution:
             self.dfs(grid,i,j+1)
             
         
+# BFS way
+class Solution:
+    #BFS way
+    def numIslands(self, grid: List[List[str]]) -> int:
+        H=len(grid)
+        if H == 0:return 0
+        W=len(grid[0])
+        if W == 0: return 0
+        count=0
+        visit=set()
+        def bfs(i,j):
+            q=collections.deque()
+            visit.add((i,j))  # as tuple 
+            q.append((i,j))  # as tuple 
+            while q:
+                row,col = q.popleft()
+                directions =[[1,0],[-1,0],[0,1],[0,-1]]
+                for dr,dc in directions:
+                    i= row+dr
+                    j= col+dc
+
+                    if (i in range(H) and j in range(W) and grid[i][j] == '1' and (i,j) not in visit):
+                        visit.add((i,j))  # as tuple 
+                        q.append((i,j))  # as tuple 
+                        
+        for i in range(0,H):
+            for j in range(0,W):
+                if grid[i][j] == '1' and (i,j) not in visit:
+                    bfs(i,j)
+                    count=count+1
+                    
+        return count
