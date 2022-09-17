896bbff7a40248165c76888a62166231d734d665
rupaku <rupakumari201@yahoo.com>
2022-09-17 11:07:08 +0530

    remove duplicates

--- Topic Wise Learning/01.Arrays/Questions/Easy/03.RemoveDeuplicatesFromSortedArray.py
+++ Topic Wise Learning/01.Arrays/Questions/Easy/03.RemoveDeuplicatesFromSortedArray.py
@@ -1,10 +1,23 @@
 '''
 https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+time  O(n)
+space O(1)
 '''
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        for i in range(len(nums)-1,0,-1):
-            if nums[i-1] == nums[i]:
-                nums.pop(i)
+#         for i in range(len(nums)-1,0,-1):
+#             if nums[i-1] == nums[i]:
+#                 nums.pop(i)
 
-        return len(nums)
+#         return len(nums)
+      if len(nums) == 0:
+         return 0
+      i=0
+      for j in range(1,len(nums)):
+            if nums[j] != nums[i]:
+                i=i+1
+                nums[i] =nums[j]
+      return i +1
+            
+            
+                
