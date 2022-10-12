cf00e7d690cc6b3bc1648eeae4601a5d15fe0989
rupaku <rupakumari201@yahoo.com>
2022-10-12 22:05:41 +0530

    string questions

--- CodeHelp/String/07.permutation.py
+++ CodeHelp/String/07.permutation.py
@@ -1,3 +1,12 @@
 '''
 https://leetcode.com/problems/permutation-in-string/solution/
 '''
+from collections import Counter
+d1 = Counter(s1)
+k = len(s1)
+for i in range(len(s2)):  # ---- O(n)
+    sub = s2[i:i+k]  # ------ O(k)
+    d2 = Counter(sub) # --- O(k)
+    if d1 == d2:
+        return True
+return False
--- CodeHelp/String/08.compression.py
+++ CodeHelp/String/08.compression.py
@@ -1,3 +1,26 @@
 '''
 https://leetcode.com/problems/string-compression/
 '''
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        
+        left, index = 0, 0
+        
+        while index < len(chars):
+		
+            chars[left] = chars[index]
+            count = 1
+			
+            while index < len(chars) - 1 and chars[index] == chars[index + 1]:
+                index += 1
+                count += 1
+			
+            if count > 1:
+                for char in str(count):
+                    left += 1
+                    chars[left] = char
+            
+            index += 1
+            left += 1
+        
+        return left
