454e60b2ff33916c317b6b7487740235a33c4dcb
rupaku <rupakumari201@yahoo.com>
2023-08-08 22:03:22 +0530

    Day 10

---
+++ Practice/17.Add_binary.py
@@ -0,0 +1,12 @@
+'''https://leetcode.com/problems/add-binary/'''
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        x=int(a,2)
+        y=int(b,2)
+        while y:
+            ans=x^y
+            carry=(x&y) << 1
+            x,y=ans,carry
+            
+        return bin(x)[2:]
+        
