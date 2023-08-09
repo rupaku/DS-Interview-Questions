5880403ce473b026b9fc9bf647becc014fd82a60
rupaku <rupakumari201@yahoo.com>
2023-08-09 21:26:39 +0530

    Day 12

---
+++ Practice/19.Middle_LL.py
@@ -0,0 +1,18 @@
+'''https://leetcode.com/problems/middle-of-the-linked-list/'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        slowPtr=head
+        fastPtr=head
+        
+        if(head):
+            while fastPtr!=None and fastPtr.next!=None :
+                slowPtr = slowPtr.next
+                fastPtr = fastPtr.next.next    
+
+        return slowPtr        
