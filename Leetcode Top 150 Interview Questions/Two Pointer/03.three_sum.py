007c78ddbd0b1b5f149efa284ff6f48e92eb34b1
rupaku <rupakumari201@yahoo.com>
2024-07-02 15:55:05 +0530

    two pointers

---
+++ Leetcode Top 150 Interview Questions/Two Pointer/03.three_sum.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/3sum/description/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res=set()
+        for i in range(len(nums)):
+            low=i+1
+            high=len(nums)-1
+            target=0-nums[i]
+            while low < high:
+                num=nums[low]+nums[high]
+                if num == target:
+                    res.add((nums[i],nums[low],nums[high]))
+                    low=low+1
+                    high=high-1
+                elif num < target:
+                    low=low+1
+                else:
+                    high=high-1
+        return list(res)
