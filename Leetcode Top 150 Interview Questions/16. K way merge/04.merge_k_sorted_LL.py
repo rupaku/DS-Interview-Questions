58dbf5c7a3b516b5f654bef0f1d11c022bbd2c9e
Rupa Kumari <rupakumari201@yahoo.com>
2024-08-13 15:55:17 +0530

    updated

--- Leetcode Top 150 Interview Questions/16. K way merge/04.merge_k_sorted_LL.py
+++ Leetcode Top 150 Interview Questions/16. K way merge/04.merge_k_sorted_LL.py
@@ -1,19 +1,28 @@
 # https://leetcode.com/problems/merge-k-sorted-lists/
 
+class HeapNode:
+    def __init__(self, node):
+        self.node = node
+
+
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    def mergeKLists(self, lists):
+        dummy = ListNode(0)
+        current = dummy
         heap = []
-        res = ListNode()
-        for i, list in enumerate(lists):
-            if list: 
-                heappush(heap, (list.val, i, list))
 
-        cur = res
+        # Initialize the heap
+        for l in lists:
+            if l:
+                heapq.heappush(heap, HeapNode(l))
+
+        # Extract the minimum node and add its next node to the heap
         while heap:
-            _, i, list = heappop(heap)
-            if list.next:
-                heappush(heap, (list.next.val, i, list.next))
+            heap_node = heapq.heappop(heap)
+            node = heap_node.node
+            current.next = node
+            current = current.next
+            if node.next:
+                heapq.heappush(heap, HeapNode(node.next))
 
-            cur.next, cur = list, list
-
-        return res.next
+        return dummy.next
