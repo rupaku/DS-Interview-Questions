09928aa3d921dceec1a3fc5c3bf572cd675edfe2
Rupa Kumari <rupakumari201@yahoo.com>
2024-08-13 10:23:19 +0530

    DP

---
+++ Leetcode Top 150 Interview Questions/26.Dynamic Programming/04.longest_common_subseq.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/longest-common-subsequence/
+
+
+class Solution:
+    def longestCommonSubsequence(self, t1: str, t2: str) -> int:
+        if len(t2) < len(t1):
+            t1,t2=t2,t1
+
+        prev=[0]*(len(t1)+1)
+        curr=[0]*(len(t1)+1)
+
+        for i in reversed(range(len(t2))):
+            for j in reversed(range(len(t1))):
+                if t2[i] == t1[j]:
+                    curr[j]=1+prev[j+1]
+                else:
+                    curr[j]=max(prev[j],curr[j+1])
+            prev,curr=curr,prev
+        return prev[0]
