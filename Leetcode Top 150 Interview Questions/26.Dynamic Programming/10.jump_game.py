fd97e1de9db5392efced1f7e6a9941ff716eec62
Rupa Kumari <rupakumari201@yahoo.com>
2024-08-14 08:45:56 +0530

    DP

---
+++ Leetcode Top 150 Interview Questions/26.Dynamic Programming/10.jump_game.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/jump-game/
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        lastPos = len(nums) - 1
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= lastPos:
+                lastPos = i
+        return lastPos == 0
