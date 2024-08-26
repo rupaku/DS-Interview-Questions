666fb89e639acb278ab3ed3c77d42a92257a796d
Rupa Kumari <rupakumari201@yahoo.com>
2024-08-26 11:48:49 +0530

    bst

--- Leetcode Top 150 Interview Questions/12.Binary search tree/02.Kth_smallest_BST.py
+++ Leetcode Top 150 Interview Questions/12.Binary search tree/02.Kth_smallest_BST.py
@@ -12,3 +12,38 @@ class Solution:
             return inorder(r.left)+[r.val]+inorder(r.right) if r else []
         return inorder(root)[k-1]
         
+
+# class TreeNode:
+#     def __init__(self,val):
+#         self.val=val
+#         self.left=None
+#         self.right=None
+        
+# class Solution:
+#     def insert(root, x):
+    
+#         if (root == None):
+#             return TreeNode(x)
+#         if (x < root.val):
+#             root.left = Solution.insert(root.left, x)
+#         elif (x > root.val):
+#             root.right = Solution.insert(root.right, x)
+#         return root
+    
+#     def inorder(self,root):
+#         return inorder(root.left)+[root.val]+ inorder(root.right) if root else []
+        
+#     def kth_smallest(self, root , k):
+#         return inorder(root)[k-1]
+        
+# if __name__ == "__main__":
+#     ls = [3,1,4,None,2]
+#     root= None
+#     k = 1
+#     obj= Solution()
+#     for x in ls:
+#         root=Solution.insert(root,x)
+#     print(root)
+        
+#     res=obj.kth_smallest(root,k)
+#     print(res)
