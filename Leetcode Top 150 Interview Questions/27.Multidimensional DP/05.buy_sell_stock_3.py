6741e24d73bf0e82c757dcd27a217de06867e047
Rupa Kumari <rupakumari201@yahoo.com>
2024-08-28 12:01:34 +0530

    buy and sell stock

--- Leetcode Top 150 Interview Questions/27.Multidimensional DP/05.buy_sell_stock_3.py
+++ Leetcode Top 150 Interview Questions/27.Multidimensional DP/05.buy_sell_stock_3.py
@@ -14,3 +14,28 @@ class Solution(object):
             t2_profit = max(t2_profit, price - t2_cost)
 
         return t2_profit
+
+
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price =float('inf')
+        max_pr =0
+        for i in range(len(prices)):
+            if prices[i] < min_price:
+                min_price=prices[i]
+            elif prices[i] - min_price > max_pr:
+                max_pr = prices[i] - min_price
+        return max_pr
+
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        ans = 0
+        for i in range(1,len(prices)):
+            if prices[i] - prices[i-1] >0:
+                ans+=(prices[i] - prices[i-1])
+        return ans
+        
