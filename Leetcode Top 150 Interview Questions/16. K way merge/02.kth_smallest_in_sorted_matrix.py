a921e54db08741a4f3cc39de00b33d22f09fd439
Rupa Kumari <rupakumari201@yahoo.com>
2024-08-28 18:37:48 +0530

    updated

--- Leetcode Top 150 Interview Questions/16. K way merge/02.kth_smallest_in_sorted_matrix.py
+++ Leetcode Top 150 Interview Questions/16. K way merge/02.kth_smallest_in_sorted_matrix.py
@@ -1,35 +1,47 @@
 # https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
 
 
-import heapq
-
 class Solution:
+    
+    def countLessEqual(self, matrix, mid, smaller, larger):
+        
+        count, n = 0, len(matrix)
+        row, col = n - 1, 0
+        
+        while row >= 0 and col < n:
+            if matrix[row][col] > mid:
+               
+                # As matrix[row][col] is bigger than the mid, let's keep track of the
+                # smallest number greater than the mid
+                larger = min(larger, matrix[row][col])
+                row -= 1
+                
+            else:
+                
+                # As matrix[row][col] is less than or equal to the mid, let's keep track of the
+                # biggest number less than or equal to the mid
+                
+                smaller = max(smaller, matrix[row][col])
+                count += row + 1
+                col += 1
+
+        return count, smaller, larger
+    
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         
-        # The size of the matrix
-        N = len(matrix)
-        
-        # Preparing our min-heap
-        minHeap = []
-        for r in range(min(k, N)):
-            
-            # We add triplets of information for each cell
-            minHeap.append((matrix[r][0], r, 0))
-        
-        # Heapify our list
-        heapq.heapify(minHeap)    
-        
-        # Until we find k elements
-        while k:
-            
-            # Extract-Min
-            element, r, c = heapq.heappop(minHeap)
-            
-            # If we have any new elements in the current row, add them
-            if c < N - 1:
-                heapq.heappush(minHeap, (matrix[r][c+1], r, c+1))
-            
-            # Decrement k
-            k -= 1
-        
-        return element  
+        n = len(matrix)
+        start, end = matrix[0][0], matrix[n - 1][n - 1]
+        while start < end:
+            mid = start + (end - start) / 2
+            smaller, larger = (matrix[0][0], matrix[n - 1][n - 1])
+
+            count, smaller, larger = self.countLessEqual(matrix, mid, smaller, larger)
+
+            if count == k:
+                return smaller
+            if count < k:
+                start = larger  # search higher
+            else:
+                end = smaller  # search lower
+
+        return start
